a75fc05f2e326a8e9fec1e2e2892606daa923446
Yousef <yous3fnoor@gmail.com>
2023-12-17 20:03:21 -0600

    Home Stretch should be the last one before finish

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var playerCard = "card7"
+    var cpuCard = "card13"
+    
+    var playerScore = 0
+    var cpuScore = 0
+    
     var body: some View {
         
         ZStack{
@@ -20,13 +27,19 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Image("button")
+                
+                Button {
+                    deal()
+                }label: {
+                    Image("button")
+                }
+                
                 Spacer()
                 HStack{
                     Spacer()
@@ -35,7 +48,7 @@ struct ContentView: View {
                             .font(.headline)
                             .padding(.bottom, 10.0)
                         
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -43,7 +56,7 @@ struct ContentView: View {
                         Text("CPU")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -55,6 +68,10 @@ struct ContentView: View {
         }
         
         
+    }
+    
+    func deal(){
+        print("Deal cards")
     }
 }
 
