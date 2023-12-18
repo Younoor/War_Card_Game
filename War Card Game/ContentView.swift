8df70af9ae0c18d3c4b3e7969f2580de50d7cd88
Yousef <yous3fnoor@gmail.com>
2023-12-17 20:35:40 -0600

    Finished app and function

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,27 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var playerCard = "card7"
-    var cpuCard = "card13"
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
     
-    var playerScore = 0
-    var cpuScore = 0
+    @State var playerScore = 0
+    @State var cpuScore = 0
     
     var body: some View {
         
         ZStack{
             
             Image("background-cloth")
+                .resizable()
+                .ignoresSafeArea()
             
             VStack{
                 Spacer()
                 Image("logo")
+                    .shadow(radius: 15)
                 Spacer()
                 HStack{
                     Spacer()
                     Image(playerCard)
+                        .shadow(radius: 15)
                     Spacer()
                     Image(cpuCard)
+                        .shadow(radius: 15)
                     Spacer()
                 }
                 Spacer()
@@ -38,6 +43,7 @@ struct ContentView: View {
                     deal()
                 }label: {
                     Image("button")
+                        .shadow(radius: 15)
                 }
                 
                 Spacer()
@@ -71,7 +77,30 @@ struct ContentView: View {
     }
     
     func deal(){
-        print("Deal cards")
+        // Randomize the players card
+        var playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // Randomize the cpus card
+        var cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // Update the score
+        if playerCardValue > cpuCardValue{
+            //Add 1 to player score
+            playerScore += 1
+        } else if cpuCardValue > playerCardValue {
+            //Add 1 to cpu score
+            cpuScore += 1
+        } else {
+            //Tie
+            playerScore += 1
+            cpuScore += 1
+        }
+        if (playerScore >= 16) || (cpuScore >= 16) {
+            playerScore = 0
+            cpuScore = 0
+        }
     }
 }
 
